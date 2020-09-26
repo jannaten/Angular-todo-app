8fd032fedcfcbd43ea84fe967725b3020579a452
Jannaten Nayem <jannaten.nayem@eng.tamk.fi>
2020-09-27 02:15:17 +0300

    Some codes added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,14 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'angularChess';
+  name: string = 'angularChess';
+  constructor() {
+    this.onNameChange('Jannaten');
+  }
+  onNameChange = (name: string): void => {
+    this.name = name;
+  };
 }
