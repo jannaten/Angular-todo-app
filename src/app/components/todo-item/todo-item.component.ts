9f8315ced1913800f6f12cb9c81d5fd999ec2ef7
Jannaten Nayem <jannaten.nayem@eng.tamk.fi>
2020-09-27 11:59:02 +0300

    Lots of modification has done

--- src/app/components/todo-item/todo-item.component.ts
+++ src/app/components/todo-item/todo-item.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
 import { Todo } from 'src/app/models/Todo';
+import { TodoService } from 'src/app/services/todo.service';
 
 @Component({
   selector: 'app-todo-item',
@@ -8,7 +9,8 @@ import { Todo } from 'src/app/models/Todo';
 })
 export class TodoItemComponent implements OnInit {
   @Input() todo: Todo;
-  constructor() {}
+  @Output() deleteTodo: EventEmitter<Todo> = new EventEmitter();
+  constructor(private todoService: TodoService) {}
 
   ngOnInit(): void {}
 
@@ -19,4 +21,15 @@ export class TodoItemComponent implements OnInit {
     };
     return classes;
   };
+
+  onToggle = (todo) => {
+    todo.completed = !todo.completed;
+    this.todoService.toggleCompleted(todo).subscribe((todo) => {
+      console.log(todo);
+    });
+  };
+
+  onDelete(todo) {
+    this.deleteTodo.emit(todo);
+  }
 }
--- src/app/components/todo/todo.component.ts
+++ src/app/components/todo/todo.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Todo } from '../../models/Todo';
+import { TodoService } from '../../services/todo.service';
 @Component({
   selector: 'app-todo',
   templateUrl: './todo.component.html',
@@ -7,25 +8,15 @@ import { Todo } from '../../models/Todo';
 })
 export class TodoComponent implements OnInit {
   todos: Todo[];
-  constructor() {}
+  constructor(private todoService: TodoService) {}
 
   ngOnInit(): void {
-    this.todos = [
-      {
-        id: 1,
-        title: 'Todo One',
-        completed: false,
-      },
-      {
-        id: 2,
-        title: 'Todo two',
-        completed: true,
-      },
-      {
-        id: 3,
-        title: 'Todo three',
-        completed: false,
-      },
-    ];
+    this.todoService.getTodos().subscribe((todos) => {
+      this.todos = todos;
+    });
+  }
+
+  deleteTodo(todo: Todo) {
+    this.todos = this.todos.filter((t) => t.id !== todo.id);
   }
 }
